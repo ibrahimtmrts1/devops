19bb984db6da5205db5891fa83d9cb5a680323dd
ibrahimtmrts1 <152934141+ibrahimtmrts1@users.noreply.github.com>
2024-02-01 02:59:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template,request
 app=Flask(__name__)
  
 @app.route('/')
-def entry_page()->'html':
+def entry_page():
     return render_template('einstein.html',page_title='Wellcome to Little Einstein Project')
  
  
 @app.route('/sum',methods=['POST'])
-def sum()->'html':
+def sum():
     x=int(request.form['firstValue'])
     y=int(request.form['secondValue'])
     return render_template('result.html',page_title='Calculation result',sum_result=(x+y),first_value=x,second_value=y,)
